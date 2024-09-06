60e8530e215205fd65b131f7919a698cf8b178b1
Mojammel Noman <molla.noman23@gmail.com>
2024-09-06 18:09:19 +0600

    add loading component

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { Navigate } from "react-router-dom";
+import Loading from "../components/Loading";
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useContext(AppContext);
 
   if (loading) {
-    return <h1>Loadinng.....</h1>;
+    return <Loading />;
   }
 
   // if user exist go to children component else login
