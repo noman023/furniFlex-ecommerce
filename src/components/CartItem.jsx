6e6f3e313f73b9f3e1e0f0fd37e8ae4d10cca0e3
Mojammel Noman <molla.noman23@gmail.com>
2024-09-07 00:58:30 +0600

    redesign cartitem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,19 +3,21 @@ import { RxCross1 } from "react-icons/rx";
 export default function CartItem() {
   return (
     <div className="flex gap-5 text-black border px-2 py-3">
-      {/* quantity */}
+      {/* manage quantity  */}
       <div className="flex gap-3 items-center text-4xl">
         <button className="hover:text-gray-500" title="Increase Quantity">
           +
         </button>
-        <p className="text-2xl border py-1 px-4">2</p>
+
+        <p className="text-2xl border py-2 px-4">2</p>
+
         <button className="hover:text-gray-500" title="Decrease Quantity">
           -
         </button>
       </div>
 
-      {/* items info */}
-      <div className="flex gap-2">
+      {/* item info */}
+      <div className="w-full flex gap-2">
         <div>
           <img
             src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
@@ -24,7 +26,7 @@ export default function CartItem() {
           />
         </div>
 
-        <div className="flex justify-between border">
+        <div className="w-full flex justify-between">
           <h1 className="text-xl font-semibold">Product name</h1>
 
           <div className="flex flex-col justify-between text-xl">
