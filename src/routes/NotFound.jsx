1184d4797d8d1237ff9daf5de88b3aaea2b5f9d6
Mojammel Noman <molla.noman23@gmail.com>
2024-09-06 17:57:53 +0600

    show notfound component in the middle

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="max-w-96 mx-auto">
-      <h1 className="text-3xl text-center">404, Page Not Found</h1>
+    <div className="h-screen flex flex-col justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl mb-3">404, Page Not Found!</h1>
 
-      <Link to={"/"}>
-        <button className="border">to home</button>
-      </Link>
+        <Link to={"/"}>
+          <button className="btn btn-primary">Go Home</button>
+        </Link>
+      </div>
     </div>
   );
 }
